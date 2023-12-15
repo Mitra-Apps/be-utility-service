110dd7b613120128a2a0e9ddc2748c0752ba0cb2
fazarrahman <fazar.rahman@gmail.com>
2023-12-15 11:28:41 +0700

    Modify docker compose and add jenkinsfile

--- main.go
+++ main.go
@@ -1,39 +1,105 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"os"
 
-	"github.com/Mitra-Apps/be-utility-service/config/postgre"
-	imagePostgreRepo "github.com/Mitra-Apps/be-utility-service/domain/image/repository/postgre"
-	pb "github.com/Mitra-Apps/be-utility-service/domain/proto/image"
-	grpcRoute "github.com/Mitra-Apps/be-utility-service/handler/grpc"
-	"github.com/Mitra-Apps/be-utility-service/service"
+	"github.com/Mitra-Apps/be-user-service/config/postgre"
+	pb "github.com/Mitra-Apps/be-user-service/domain/proto/user"
+	userPostgreRepo "github.com/Mitra-Apps/be-user-service/domain/user/repository/postgre"
+	grpcRoute "github.com/Mitra-Apps/be-user-service/handler/grpc"
+	"github.com/Mitra-Apps/be-user-service/service"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"go.elastic.co/apm/module/apmgrpc"
 
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
 )
 
-type server struct {
-	pb.UnimplementedImageServiceServer
-}
-
 func main() {
-	lis, err := net.Listen("tcp", os.Getenv("APP_PORT"))
+	ctx := context.Background()
+
+	godotenv.Load()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", os.Getenv("GRPC_PORT")))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	db := postgre.Connection()
-	usrRepo := imagePostgreRepo.NewPostgre(db)
+	usrRepo := userPostgreRepo.NewPostgre(db)
 	svc := service.New(usrRepo)
-	grpcServer := grpc.NewServer()
+	grpcServer := GrpcNewServer(ctx, []grpc.ServerOption{})
 	route := grpcRoute.New(svc)
-	pb.RegisterImageServiceServer(grpcServer, route)
+	pb.RegisterUserServiceServer(grpcServer, route)
 
-	fmt.Printf("GRPC Server listening at %v ", lis.Addr())
-	if err = grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v \n", err)
+	go func() {
+		<-ctx.Done()
+		grpcServer.GracefulStop()
+	}()
+
+	go HttpNewServer(ctx, os.Getenv("GRPC_PORT"), os.Getenv("HTTP_PORT"))
+
+	grpcServer.Serve(lis)
+}
+
+func GrpcNewServer(ctx context.Context, opts []grpc.ServerOption) *grpc.Server {
+	logrusEntry := logrus.NewEntry(logrus.StandardLogger())
+	logrusOpts := []grpc_logrus.Option{
+		grpc_logrus.WithLevels(grpc_logrus.DefaultCodeToLevel),
 	}
+	grpc_logrus.ReplaceGrpcLogger(logrusEntry)
+
+	opts = append(opts, grpc.StreamInterceptor(
+		grpc_middleware.ChainStreamServer(
+			grpc_ctxtags.StreamServerInterceptor(),
+			grpc_logrus.StreamServerInterceptor(logrusEntry, logrusOpts...),
+			grpc_recovery.StreamServerInterceptor(),
+			apmgrpc.NewStreamServerInterceptor(apmgrpc.WithRecovery()),
+		)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_ctxtags.UnaryServerInterceptor(),
+			grpc_logrus.UnaryServerInterceptor(logrusEntry, logrusOpts...),
+			grpc_recovery.UnaryServerInterceptor(),
+			apmgrpc.NewUnaryServerInterceptor(apmgrpc.WithRecovery()),
+		)),
+	)
+
+	myServer := grpc.NewServer(opts...)
+
+	reflection.Register(myServer)
+	return myServer
+}
+
+func HttpNewServer(ctx context.Context, grpcPort, httpPort string) error {
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	if err := pb.RegisterUserServiceHandlerFromEndpoint(ctx, mux, fmt.Sprintf("localhost:%s", grpcPort), opts); err != nil {
+		return err
+	}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", httpPort),
+		Handler: mux,
+	}
+
+	go func() {
+		<-ctx.Done()
+		if err := srv.Shutdown(ctx); err != nil {
+			logrus.Panicf("failed to shutdown server: %v", err)
+		}
+	}()
+
+	return srv.ListenAndServe()
 }
